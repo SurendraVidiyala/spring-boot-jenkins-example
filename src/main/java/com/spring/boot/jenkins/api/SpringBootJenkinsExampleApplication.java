7334d25e9cbba349c41164a2448a05a8d8a7b94f
Surendra Vidiyala <vidiyala.surendra@gmail.com>
2022-07-16 22:51:51 -0400

    added the dependencies for junit 5

--- src/main/java/com/spring/boot/jenkins/api/SpringBootJenkinsExampleApplication.java
+++ src/main/java/com/spring/boot/jenkins/api/SpringBootJenkinsExampleApplication.java
@@ -1,10 +1,23 @@
 package com.spring.boot.jenkins.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+/**
+ * @author suryasurenchowdary
+ */
 @SpringBootApplication
 public class SpringBootJenkinsExampleApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsExampleApplication.class);
+	@PostConstruct
+	public void init(){
+		logger.info("Application Started ::::");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsExampleApplication.class, args);
