7ab412b5aee2744bfa1d26da495beeff6ab87e40
Surendra Vidiyala <vidiyala.surendra@gmail.com>
2022-07-16 22:32:08 -0400

    Basic structure of the spring-boot application created

---
+++ src/main/java/com/spring/boot/jenkins/example/SpringBootJenkinsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.jenkins.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/jenkins/example/SpringBootJenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.jenkins.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
